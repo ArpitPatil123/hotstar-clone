4f23c1ff3e1882aa07121af9c8f87249ffe57789
ArpitPatil <arpitpatilap122833@gmail.com>
2023-06-24 18:06:34 +0530

    code-updated

--- src/API/requests.jsx
+++ src/API/requests.jsx
@@ -1,4 +1,4 @@
-const apiKey = "e07f5da07b3e1be9c5f23588cc5eae82";
+const apiKey = process.env.REACT_APP_API_KEY;
 
 const requests = {
   // fetchTrending: `/trending/all/week?api_key=${apiKey}&language=en-US`,
@@ -10,8 +10,6 @@ const requests = {
   fetchDrama: `/discover/tv?api_key=${apiKey}&language=hi-IN&region=IN&sort_by=popularity.desc&with_original_language=hi&with_genres=18`,
 };
 
-// https://api.themoviedb.org/3/discover/movie?api_key=e07f5da07b3e1be9c5f23588cc5eae82&language=hi-IN&region=IN&sort_by=popularity.desc&page=1&primary_release_year=2018&with_original_language=hi
+// https://api.themoviedb.org/3/discover/movie?api_key={your_api_key}=hi-IN&region=IN&sort_by=popularity.desc&page=1&primary_release_year=2018&with_original_language=hi
 
 export default requests;
-
-//https://api.themoviedb.org/3/movie/783461?api_key=e07f5da07b3e1be9c5f23588cc5eae82
